3b9bbe0ac51cbb3933d0535f21572e61bc35aa5b
torbjorn josefsson <torbjorn.josefsson@storebrand.no>
2015-03-12 10:30:50 +0100

    blah

--- js/react/barchart.js
+++ js/react/barchart.js
@@ -1,82 +1,86 @@
 /** @jsx React.DOM */
 
 
- var SetIntervalMixin = {
-                componentWillMount: function() {
-                    this.intervals = [];
-                },
-                setInterval: function() {
-                    this.intervals.push(setInterval.apply(null, arguments));
-                },
-                componentWillUnmount: function() {
-                    this.intervals.map(clearInterval);
-                }
-            };
+var SetIntervalMixin = {
+  componentWillMount: function () {
+    this.intervals = [];
+  },
+  setInterval: function () {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function () {
+    this.intervals.map(clearInterval);
+  }
+};
 
 
-            var BarChart = ReactD3.BarChart;
+var BarChart = ReactD3.BarChart;
 
-            var barChartData = [{
-                label:'somethingA',
-                values: [{x:'Monday', y:10}, {x:'Tuesday', y:4}, {x:'Wednesday', y:3},{x:'Thursday', y:3},{x:'Friday', y:3},{x:'Saturday', y:3},{x:'Sunday', y:3}]
-            }];
+var barChartData = [{
+  label: 'somethingA',
+  values: [{x: 'Monday', y: 10}, {x: 'Tuesday', y: 4}, {x: 'Wednesday', y: 3}, {x: 'Thursday', y: 3}, {
+    x: 'Friday',
+    y: 3
+  }, {x: 'Saturday', y: 3}, {x: 'Sunday', y: 3}]
+}];
 
-            var mountPoint = document.getElementById('barchart');
+var mountPoint = document.getElementById('barchart');
 
-            var BarChartComponent = React.createClass({
-                mixins: [SetIntervalMixin],
+var BarChartComponent = React.createClass({
+  mixins: [SetIntervalMixin],
 
-                handleResize: function(e) {
-                    this.setState({
-                        width: mountPoint.offsetWidth,
-                        height: mountPoint.offsetHeight
-                    });
-                },
+  handleResize: function (e) {
+    this.setState({
+      width: mountPoint.offsetWidth,
+      height: mountPoint.offsetHeight
+    });
+  },
 
-                loadChartDataFromServer: function() {
-                    $.ajax({
-                        url: this.props.url,
-                        dataType: 'json',
-                        success: function(data) {
-                            if (this.isMounted()) {
-                                var blah = _.clone(barChartData);
-                                blah[0].values[0].y = ++counter;
-                                this.setState({barChartData: blah});
-                            }
-                        }.bind(this),
-                        error: function(xhr, status, err) {
-                            console.error(this.props.url, status, err.toString());
-                        }.bind(this)
-                    });
-                },
+  loadChartDataFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function (data) {
+        if (this.isMounted()) {
+          var blah = _.clone(barChartData);
+          blah[0].values[0].y = ++counter;
+          this.setState({barChartData: blah});
+        }
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
-                componentDidMount: function(){
-                    this.setState({
-                        width: mountPoint.offsetWidth,
-                        height: mountPoint.offsetHeight
-                    });
-                    this.setInterval(this.loadChartDataFromServer, this.props.pollInterval);
-                     window.addEventListener('resize', this.handleResize);
-                },
-                componentWillUnmount: function() {
-                    window.removeEventListener('resize', this.handleResize);
-                },
+  componentDidMount: function () {
+    this.setState({
+      width: mountPoint.offsetWidth,
+      height: mountPoint.offsetHeight
+    });
+    this.setInterval(this.loadChartDataFromServer, this.props.pollInterval);
+    window.addEventListener('resize', this.handleResize);
+  },
+  componentWillUnmount: function () {
+    window.removeEventListener('resize', this.handleResize);
+  },
 
-                getInitialState:function(){
-                    return {
-                        barChartData: barChartData,
-                        width: 650,
-                        height: 300
-                    }
-                },
-                render: function() {
-                    return (
-                            <BarChart   data={this.state.barChartData}
-                                        width={this.state.width}
-                                        height={this.state.height}
-                                        yAxis={{label: "hits"}}
-                                        margin={{top:50, bottom:50, left:50, right:50}}/>
-                    );
-                }
-            });
-            React.render(<BarChartComponent url="http://date.jsontest.com" pollInterval={2000} />,mountPoint);
+  getInitialState: function () {
+    return {
+      barChartData: barChartData,
+      width: 650,
+      height: 300
+    }
+  },
+  render: function () {
+    return (
+      <BarChart   data={this.state.barChartData}
+        width={this.state.width}
+        height={this.state.height}
+        yAxis={{label: "hits"}}
+        margin={{top: 50, bottom: 50, left: 50, right: 50}}/>
+    );
+  }
+});
+
+React.render(<BarChartComponent url="http://date.jsontest.com" pollInterval={2000} />, mountPoint);
--- js/react/linechart.js
+++ js/react/linechart.js
@@ -1,84 +1,84 @@
 /** @jsx React.DOM */
 
- var LineChart = ReactD3.LineChart;
+var LineChart = ReactD3.LineChart;
 
-            lineChartData = [
-                {
-                    label: 'somethingA',
-                    values: [{x: 0, y: 2}, {x: 1.3, y: 5}, {x: 3, y: 6}]
-                },
-                {
-                    label: 'somethingB',
-                    values: [{x: 0, y: 3}, {x: 1.3, y: 4}, {x: 3, y: 7}]
-                }
-            ];
-            var counter = 2;
-            var lineChartMountPoint = document.getElementById('linechart');
+lineChartData = [
+  {
+    label: 'somethingA',
+    values: [{x: 0, y: 2}, {x: 1.3, y: 5}, {x: 3, y: 6}]
+  },
+  {
+    label: 'somethingB',
+    values: [{x: 0, y: 3}, {x: 1.3, y: 4}, {x: 3, y: 7}]
+  }
+];
+var counter = 2;
+var lineChartMountPoint = document.getElementById('linechart');
 
-            var colorScale = d3.scale.ordinal()
-                    .domain(["somethingA", "somethingB"])
-                    .range(["#404040", "#819f2b"]);
+var colorScale = d3.scale.ordinal()
+  .domain(["somethingA", "somethingB"])
+  .range(["#404040", "#819f2b"]);
 
-            var LineChartComponent = React.createClass({
+var LineChartComponent = React.createClass({
 
-                loadChartDataFromServer: function() {
-                    $.ajax({
-                        url: this.props.url,
-                        dataType: 'json',
-                        success: function(data) {
-                            var blah = _.clone(lineChartData);
-                            blah[0].values[0].y = ++counter;
-                            if (this.isMounted()) {
-                                this.setState({lineChartData: blah});
-                            }
-                        }.bind(this),
-                        error: function(xhr, status, err) {
-                            console.error(this.props.url, status, err.toString());
-                        }.bind(this)
-                    });
-                },
-                
-                handleResize: function(e) {
-                    this.setState({
-                        width: lineChartMountPoint.offsetWidth,
-                        height: lineChartMountPoint.offsetHeight
-                    });
-                },
+  loadChartDataFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function (data) {
+        var blah = _.clone(lineChartData);
+        blah[0].values[0].y = ++counter;
+        if (this.isMounted()) {
+          this.setState({lineChartData: blah});
+        }
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
 
-                componentDidMount: function(){
-                    window.addEventListener('resize', this.handleResize);
-                    this.loadChartDataFromServer();
-                    setInterval(this.loadChartDataFromServer, this.props.pollInterval);
-                    this.setState({
-                        width: lineChartMountPoint.offsetWidth,
-                        height: lineChartMountPoint.offsetHeight
-                    });
-                },
-                
-                componentWillUnmount: function() {
-                    window.removeEventListener('resize', this.handleResize);
-                },
+  handleResize: function (e) {
+    this.setState({
+      width: lineChartMountPoint.offsetWidth,
+      height: lineChartMountPoint.offsetHeight
+    });
+  },
 
-                getInitialState:function(){
-                    return {
-                        lineChartData: lineChartData,
-                        width: 500,
-                        height: 300
-                    }
-                },
+  componentDidMount: function () {
+    window.addEventListener('resize', this.handleResize);
+    this.loadChartDataFromServer();
+    setInterval(this.loadChartDataFromServer, this.props.pollInterval);
+    this.setState({
+      width: lineChartMountPoint.offsetWidth,
+      height: lineChartMountPoint.offsetHeight
+    });
+  },
 
-			    render: function() {
-			        return (
-                            <LineChart
-                                    colorScale={colorScale}
-                                    data={this.state.lineChartData}
-                                    width={this.state.width}
-                                    height={this.state.height}
-                                    xAxis={{innerTickSize: 6, label: "day"}}
-                                    yAxis={{label: "hits"}}
-                                    margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
-			            );
-			    }
-			});
+  componentWillUnmount: function () {
+    window.removeEventListener('resize', this.handleResize);
+  },
 
-            React.render(<LineChartComponent url="http://date.jsontest.com" pollInterval={5000} />, lineChartMountPoint);
+  getInitialState: function () {
+    return {
+      lineChartData: lineChartData,
+      width: 500,
+      height: 300
+    }
+  },
+
+  render: function () {
+    return (
+      <LineChart
+        colorScale={colorScale}
+        data={this.state.lineChartData}
+        width={this.state.width}
+        height={this.state.height}
+        xAxis={{innerTickSize: 6, label: "day"}}
+        yAxis={{label: "hits"}}
+        margin={{top: 10, bottom: 50, left: 50, right: 10}}/>
+    );
+  }
+});
+
+React.render(<LineChartComponent url="http://date.jsontest.com" pollInterval={5000} />, lineChartMountPoint);
